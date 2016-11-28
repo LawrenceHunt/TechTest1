fda0a3b79b3d8b4eec5dd58c86180788fd6c7aa7
Lawrence Hunt <lawrence.hunt@hotmail.com>
2016-11-28 15:54:21 +0000

    added feature test. all tests passing

--- spec/features/save_key_value_spec.rb
+++ spec/features/save_key_value_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 feature 'Save key value pair' do
+  scenario 'I can save and retrieve a key and value pair' do
   visit('/set?key1=value1')
-  
+  visit('/get?key=key1')
+  expect(page).to have_content('value1')
+  end
 end
