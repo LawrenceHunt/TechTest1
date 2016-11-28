13bec0323720da86e52c7acdf0ade00d11e325d7
Lawrence Hunt <lawrence.hunt@hotmail.com>
2016-11-28 15:50:49 +0000

    working solution using class instance variables

--- app.rb
+++ app.rb
@@ -14,45 +14,25 @@ enable :sessions
   get '/' do
   erb :'index'
   end
-
+  
   get '/set' do
     key = params.flatten[-2]
     value = params.flatten[-1]
-    session[:key] = key
-    session[:value] = value
     key_value = KeyValue.new(key, value)
     @key_value_array.array << key_value
     erb :'saved'
   end
 
   get '/get' do
-    key = params[:somekey]
-    if key = session[:key]
-      @result = session[:value]
+    key = params.flatten[-1]
+    search = @key_value_array.find_value(key)
+    if search
+      @result = search.to_s
     else
-      @result = 'FUCK OFF'
+      @result = 'No can do!'
     end
     erb :'result'
   end
-
-  #
-  # get '/set' do
-  #   key = params.flatten[-2]
-  #   value = params.flatten[-1]
-  #   key_value = KeyValue.new(key, value)
-  #   @key_value_array.array << key_value
-  #   erb :'saved'
-  # end
-  #
-  # get '/get' do
-  #   key = params[:somekey]
-  #   if key = session[:key]
-  #     @result = session[:value]
-  #   else
-  #     @result = 'FUCK OFF'
-  #   end
-  #   erb :'result'
-  # end
 
   run! if app_file == $0
 end
